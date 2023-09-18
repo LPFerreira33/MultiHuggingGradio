682c654181de0011f9b161c5c11afc36861b38d9
LPFerreira33 <32686057+LPFerreira33@users.noreply.github.com>
2023-09-18 20:45:21 +0100

    Github actions #48

--- check_versions.py
+++ check_versions.py
@@ -2,11 +2,11 @@ import torch
 import sys
 import numpy as np
 
-print('__Python VERSION:', sys.version)
 print('__pyTorch VERSION:', torch.__version__)
 print('__pyTorch CUDA VERSION:', torch.version.cuda)
 print('__CUDNN VERSION:', torch.backends.cudnn.version())
 print('__Number CUDA Devices:', torch.cuda.device_count())
+print('Device 0:', torch.cuda.get_device_name(0))
 print("OS: ", sys.platform)
 print("Python: ", sys.version)
 print("PyTorch: ", torch.__version__)
