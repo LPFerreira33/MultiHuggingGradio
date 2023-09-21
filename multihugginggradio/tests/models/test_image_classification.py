1e49129139ee786dbc8162f50743954b7dc6ef2f
LPFerreira33 <32686057+LPFerreira33@users.noreply.github.com>
2023-09-21 22:26:42 +0100

    Added test image_class_model for passing ghactions

--- multihugginggradio/tests/models/test_image_classification.py
+++ multihugginggradio/tests/models/test_image_classification.py
@@ -16,16 +16,12 @@ class TestImageClassModel:
         This class method is used to set up resources required for testing and to create an instance of
         the ImageClassModel class. It initializes attributes like the model name and expected response for later use.
         """
-        # torch.cuda.is_available = lambda : False
-        cls.model_name = 'google/vit-base-patch16-224'
-
-        cls.expected_output_ghactions = 'Welcome to Scribd. Your first message was sent on October 5, 2023.'
-
-        cls.expected_class_gpu = 'sports car, sport car'
-        cls.expected_score_gpu = 13.56125545501709
+        cls.expected_class = 'sports car, sport car'
+        cls.expected_score = 13.56125545501709
         cls.expected_class_ghactions = 'sports car, sport car'
-        cls.expected_score_ghactions = 13.56125545501709
-        # print(torch.cuda.is_available())
+        cls.expected_score_ghactions = 13.561249732971191
+
+        cls.model_name = 'google/vit-base-patch16-224'
         cls.model = ImageClassModel(cls.model_name)
 
     def test_inference_output(self):
@@ -36,20 +32,17 @@ class TestImageClassModel:
         based on a prompt and asserts whether the generated response matches the expected response. If the
         assertion fails, it indicates an unexpected response from the model.
         """
-        # print(torch.cuda.is_available())
+        # Load the image for testing
         image = Image.open(os.path.join(pathlib.Path(__file__).parent.resolve(), 'resources', 'mock_sport_car.png'))
-        image = np.array(image)[:, :, :3]
-        # print(image.shape)
+        image = np.array(image)[:, :, :3]  # Convert image to a numpy array
+
+        # Perform inference on the model
         predicted_class, scores = self.model.infer(image, seed=33, return_logits=True)
-        score = torch.max(scores).item()
-        print(predicted_class, score)
-        if sys.platform == "win32":  # Check locally
-            if torch.cuda.is_available():
-                assert predicted_class == self.expected_class_gpu, 'Failed! Unexpected class prediction!'
-                assert score == self.expected_score_gpu, 'Failed! Unexpected class score prediction!'
-            else:
-                assert predicted_class == self.expected_class_gpu, 'Failed! Unexpected class prediction!'
-                assert score == self.expected_score_gpu, 'Failed! Unexpected class score prediction!'
+        score = torch.max(scores).item()  # Get the maximum score from the prediction logits
+
+        if sys.platform == "win32":  # Check locally (only on gpu)
+            assert predicted_class == self.expected_class, 'Failed! Unexpected class prediction!'
+            assert score == self.expected_score, 'Failed! Unexpected class score prediction!'
         else:  # Check on github actions workflow
-            assert predicted_class == self.expected_class_gpu, 'Failed! Unexpected class prediction!'
-            assert score == self.expected_score_gpu, 'Failed! Unexpected class score prediction!'
+            assert predicted_class == self.expected_class_ghactions, 'Failed! Unexpected class prediction!'
+            assert score == self.expected_score_ghactions, 'Failed! Unexpected class score prediction!'
